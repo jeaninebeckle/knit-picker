bedff847d6fbc5516a14b77a14a9660ee6f3bc73
Jeanine Beckle <jeaninebeckle@gmail.com>
2020-09-11 18:30:02 -0500

    basic google login/out

---
+++ src/Components/pages/CreateProject/CreateProject.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class CreateProject extends React.Component {
+  render() {
+    return (
+      <div className="CreateProject">
+        <h1>CreateProject</h1>
+      </div>
+    );
+  }
+}
+
+export default CreateProject;
---
+++ src/Components/pages/SingleProject/SingleProject.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class SingleProject extends React.Component {
+  render() {
+    return (
+      <div className="SingleProject">
+        <h1>SingleProject</h1>
+      </div>
+    );
+  }
+}
+
+export default SingleProject;
