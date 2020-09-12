3917f227386a83229c8cec9064560f81df94e5c4
Jeanine Beckle <jeaninebeckle@gmail.com>
2020-09-11 19:53:48 -0500

    functional navbar

--- src/Components/MyNavbar/MyNavbar.js
+++ src/Components/MyNavbar/MyNavbar.js
@@ -1,20 +1,78 @@
 import React from 'react';
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
+import { NavLink as RRNavLink } from 'react-router-dom';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+} from 'reactstrap';
 import firebase from 'firebase/app';
 import 'firebase/auth';
 
 class MyNavbar extends React.Component {
+  static propTypes = {
+    authed: PropTypes.bool.isRequired,
+  }
+
+  state = {
+    isOpen: false,
+  }
+
   logoutClickEvent = (e) => {
     e.preventDefault();
     firebase.auth().signOut();
   }
 
+  toggle = () => {
+    const { isOpen } = this.state;
+    this.setState({ isOpen: !isOpen });
+  }
+
   render() {
+    const { isOpen } = this.state;
+
+    const buildNavbar = () => {
+      const { authed } = this.props;
+
+      if (authed) {
+        return (
+          <Nav className="ml-auto" navbar>
+            <NavItem>
+              <NavLink tag={RRNavLink} to='/inventory'>Inventory</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink tag={RRNavLink} to='/patterns'>Patterns</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink tag={RRNavLink} to='/projects'>Projects</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink tag={RRNavLink} to='/gallery'>Gallery</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink type="button" onClick={this.logoutClickEvent}>Log Out</NavLink>
+            </NavItem>
+          </Nav>
+        );
+      }
+
+      return <Nav className="ml-auto" navbar></Nav>;
+    };
+
     return (
-      <div className="MyNavbar">
-        <h1>MyNavbar</h1>
-        <button className="btn btn-light" onClick={this.logoutClickEvent}>Log Out</button>
-      </div>
+      <div>
+      <Navbar color="light" light expand="md">
+        <NavbarBrand href="/home">Knit Picker</NavbarBrand>
+        <NavbarToggler onClick={this.toggle} />
+        <Collapse isOpen={isOpen} navbar>
+         {buildNavbar()}
+        </Collapse>
+      </Navbar>
+    </div>
     );
   }
 }
