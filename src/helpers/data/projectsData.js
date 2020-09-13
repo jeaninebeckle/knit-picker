46d1644d9b0e3af98251700a4f6edce9bff2d862
Jeanine Beckle <jeaninebeckle@gmail.com>
2020-09-13 17:32:57 -0500

    oops worked on wrong feature

---
+++ src/helpers/data/projectsData.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+import apiKeys from '../apiKeys.json';
+import utils from '../utils';
+
+const baseUrl = apiKeys.firebaseConfig.databaseURL;
+
+const getProjectsByUid = (uid) => new Promise((resolve, reject) => {
+  axios.get(`${baseUrl}/projects.json?orderBy="uid"&equalTo="${uid}"`)
+    .then(({ data }) => resolve(utils.convertFirebaseCollection(data)))
+    .catch((err) => reject(err));
+});
+
+const getSingleProjects = (projectId) => axios.get(`${baseUrl}/projects/${projectId}.json`);
+
+export default {
+  getProjectsByUid,
+  getSingleProjects,
+};
